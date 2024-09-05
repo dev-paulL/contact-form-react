309dfb1bba5c9e9e7be2349337115a3e0f829547
PaulDev <13600558+dev-paulL@users.noreply.github.com>
2024-09-05 10:38:06 +0200

    accessibility:role=alert on the success toast, made it visible for 5 seconds instead of 3.

--- src/components/MessageSentPopup.tsx
+++ src/components/MessageSentPopup.tsx
@@ -3,7 +3,6 @@ import IconSuccessCheck from "./icons/IconSuccessCheck";
 export default function MessageSentPopup() {
   /* Animated "toast" message form submission success */
   /* Animated using framer-motion, I wanted to make it look like it's dropping from the top, I used the same animation as I used for the cart in my other project */
-
   return (
     <motion.div
       initial={{ opacity: 0, y: -200, x: "-50%" }}
@@ -11,11 +10,12 @@ export default function MessageSentPopup() {
       exit={{ opacity: 0, y: -200, x: "-50%" }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
       className="messageSentPopup"
-      aria-live="assertive" 
+      aria-live="assertive"
       aria-atomic="true"
+      role="alert"
     >
       <div>
-        <IconSuccessCheck /> <h2>Message Sent!</h2>{" "}
+        <IconSuccessCheck /> <h2>Message Sent!</h2>
       </div>
       <p>Thanks for completing the form, we'll be in touch soon!</p>
     </motion.div>
